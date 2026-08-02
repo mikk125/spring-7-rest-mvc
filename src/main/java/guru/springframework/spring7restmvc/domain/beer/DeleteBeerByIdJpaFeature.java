a3c397e66e665a177eb94d4c55a8ad914718c3d1
Mikk.Sillamaa <mikk.sillamaa@rmit.ee>
2026-08-02 21:23:33 +0300

    Added cache eviction

--- src/main/java/guru/springframework/spring7restmvc/domain/beer/DeleteBeerByIdJpaFeature.java
+++ src/main/java/guru/springframework/spring7restmvc/domain/beer/DeleteBeerByIdJpaFeature.java
@@ -2,10 +2,14 @@ package guru.springframework.spring7restmvc.domain.beer;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 
 @Slf4j
@@ -13,9 +17,17 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class DeleteBeerByIdJpaFeature {
 
+    private final CacheManager cacheManager;
     private final BeerRepository beerRepository;
 
+//    @Caching(evict = {
+//            @CacheEvict(cacheNames = "beerCache", key = "#beerId"),
+//            @CacheEvict(cacheNames = "beerListCache") // removes data from cache
+//    })
     public Boolean execute(UUID id) {
+        Objects.requireNonNull(cacheManager.getCache("beerCache")).evict(id);
+        Objects.requireNonNull(cacheManager.getCache("beerListCache")).clear(); //clear all
+
         if (beerRepository.existsById(id)) {
             beerRepository.deleteById(id);
             return true;
